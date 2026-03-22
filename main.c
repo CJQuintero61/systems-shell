415b0fe398d02fa4123bfbef5589017a894b2635
CJQuintero61 <cjquintero61@gmail.com>
2026-03-22 17:47:25 -0500

    initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+    Christian Quintero, Anointiyae Beasley, Venus Ubani, Boris Hernandez
+    Systems Programming Project - Spring 2026
+    03/22/2026
+
+    to run:
+        1. make main
+        2. ./main
+        3. make clean
+    
+    This program implements a shell in C
+*/
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World\n");
+    return 0;
+}
